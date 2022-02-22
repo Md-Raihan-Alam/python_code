3e10dab1855ab88d8672f8512247466fd8223aec
Md-Raihan-Alam <mdraihanalam40@gmail.com>
2022-02-22 15:14:09 +0600

    comment

--- Class-OOP/construction-destructor.py
+++ Class-OOP/construction-destructor.py
@@ -1,14 +1,14 @@
 class PartyAnimal:
     x = 0
 
-    def __init__(self):
+    def __init__(self):  # construction
         print("I am constructed")
 
     def party(self):
         self.x = self.x+1
         print("So far", self.x)
 
-    def __del__(self):
+    def __del__(self):  # destructions
         print("I am destructed", self.x)
 
 
